9b46b4cd3c824748f8169db1402113f9ebaed050
nyuta <pillowforneedles@gmail.com>
2020-05-14 23:17:51 +0300

    module-02 task-4

--- module-02/task-04.js
+++ module-02/task-04.js
@@ -1,7 +1,24 @@
 'use strict'
 
+const formatString = function(string) {
 
+  if (string.length <= 40) {
+    console.log (string);
+  } else if (string.length > 40) {
+    const stringCrop = string.slice(0, 40);
+  console.log (`${stringCrop}...`);
+  }
 
+};
+
+console.log(formatString('Curabitur ligula sapien, tincidunt non.'));
+// вернется оригинальная строка
+console.log(formatString('Vestibulum facilisis, purus nec pulvinar iaculis.'));
+// вернется форматированная строка
+console.log(formatString('Curabitur ligula sapien.'));
+// вернется оригинальная строка
+console.log(formatString('Nunc sed turpis. Curabitur a felis in nunc fringilla tristique.'));
+// вернется форматированная строка
 
 //
 //
