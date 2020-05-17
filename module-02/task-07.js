6b265b14fb625bf2d87db0e7d459b71a502007d4
nyuta <pillowforneedles@gmail.com>
2020-05-17 18:24:38 +0300

    module-02 task-07

--- module-02/task-07.js
+++ module-02/task-07.js
@@ -1,7 +1,5 @@
 'use strict'
 
-const login = [];
-
 const addLogin = function(allLogins, login) {
   allLogins = allLogins.includes (login);
 
@@ -41,10 +39,10 @@ const addLogin = function(allLogins, login) {
 
 };
 
-console.log(addLogin(login, 'Ajax')); // 'Логин успешно добавлен!'
-console.log(addLogin(login, 'robotGoogles')); // 'Такой логин уже используется!'
-console.log(addLogin(login, 'Zod')); // 'Ошибка! Логин должен быть от 4 до 16 символов'
-console.log(addLogin(login, 'jqueryisextremelyfast')); // 'Ошибка! Логин должен быть от 4 до 16 символов'
+console.log(addLogin(logins, 'Ajax')); // 'Логин успешно добавлен!'
+console.log(addLogin(logins, 'robotGoogles')); // 'Такой логин уже используется!'
+console.log(addLogin(logins, 'Zod')); // 'Ошибка! Логин должен быть от 4 до 16 символов'
+console.log(addLogin(logins, 'jqueryisextremelyfast')); // 'Ошибка! Логин должен быть от 4 до 16 символов'
 //
 //
 //
