7ae064567756c94c8e1079f9f842d09c8a4ba9f9
nyuta <pillowforneedles@gmail.com>
2020-07-15 00:39:21 +0300

    module-11 task-1,2

--- module-11/task-01/index.js
+++ module-11/task-01/index.js
@@ -7,6 +7,35 @@ const colors = [
   '#795548',
 ];
 
-const randomIntegerFromInterval = (min, max) => {
-  return Math.floor(Math.random() * (max - min + 1) + min);
+// const randomIntegerFromInterval = (min, max) => {
+//   return Math.floor(Math.random() * (max - min + 1) + min);
+// };
+
+const randomIntegerFromInterval = (max) => {
+  return Math.floor(Math.random() * (max + 1));
 };
+
+const startBtn = document.querySelector('button[data-action="start"]');
+const stopBtn = document.querySelector('button[data-action="stop"]');
+// const body = document.querySelector('body');
+
+const setRandomColor = () => {
+  const randomColor = colors[randomIntegerFromInterval(colors.length - 1)];
+  console.log(randomColor);
+  document.body.style.background = randomColor;
+};
+
+let timerId = null;
+
+const changeColorOfBody = () => {
+  timerId = setInterval(setRandomColor, 1000);
+  return timerId;
+}
+
+const stopColorOfBody = () => {
+ clearInterval(timerId);
+}
+
+startBtn.addEventListener('click', changeColorOfBody);
+stopBtn.addEventListener('click', stopColorOfBody);
+
